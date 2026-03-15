d7b23d4f3f48e4454d129db14b4613be4e7a6310
lnery <lnery@lamar.edu>
2026-03-15 15:34:40 -0500

    Added the Resume model that we will be working with as of right now.

---
+++ src/main/java/com/luisnery/resumemaster/model/Resume.java
@@ -0,0 +1,38 @@
+package com.luisnery.resumemaster.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "resumes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Resume {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime lastModified;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
