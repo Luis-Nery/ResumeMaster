21d9d6079da8c331986c5de50230dfae4c048f54
lnery <lnery@lamar.edu>
2026-04-10 17:12:54 -0500

    rollback

--- generate-pdf.js
+++ generate-pdf.js
@@ -17,7 +17,6 @@ const html = Buffer.from(process.argv[2], 'base64').toString('utf-8')
         format: 'A4',
         printBackground: true,
         margin: {top: 0, right: 0, bottom: 0, left: 0},
-        scale: 0.9,
     })
     await browser.close()
     process.stdout.write(pdf)
